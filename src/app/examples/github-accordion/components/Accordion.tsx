9314b4d78800adf9d8c2842eb8b2f1ea29a58b0e
stefan judis <stefanjudis@gmail.com>
2025-02-27 19:09:08 +0100

    Add accordion example

---
+++ src/app/examples/github-accordion/components/Accordion.tsx
@@ -0,0 +1,58 @@
+import { useId } from "react";
+import styles from "./Accordion.module.css";
+
+type AccordionEntry = {
+  title: string;
+  content: string;
+  img: string;
+};
+
+export const Accordion = ({ entries }: { entries: AccordionEntry[] }) => {
+  const id = useId();
+
+  return (
+    <div className={styles.accordionContainer}>
+      <div className={styles.accordion}>
+        <div className={styles.entries}>
+          {entries.map((entry, index) => (
+            <details
+              className={styles.details}
+              key={index}
+              open={index === 0}
+              name={id}
+            >
+              <summary>
+                {entry.title}
+
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  className="octicon octicon-plus"
+                  viewBox="0 0 16 16"
+                  width="16"
+                  height="16"
+                  fill="#a371f7"
+                >
+                  <path d="M7.75 2a.75.75 0 0 1 .75.75V7h4.25a.75.75 0 0 1 0 1.5H8.5v4.25a.75.75 0 0 1-1.5 0V8.5H2.75a.75.75 0 0 1 0-1.5H7V2.75A.75.75 0 0 1 7.75 2Z"></path>
+                </svg>
+              </summary>
+              <p>{entry.content}</p>
+            </details>
+          ))}
+        </div>
+        <div className={styles.images}>
+          {entries.map((entry, index) => (
+            <img
+              key={index}
+              src={entry.img}
+              alt=""
+              className={styles.image}
+              height="1000"
+              width="1000"
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/app/examples/github-accordion/page.tsx
@@ -0,0 +1,42 @@
+import { Accordion } from "./components/Accordion";
+import styles from "./page.module.css";
+
+export default function GitHubExample() {
+  return (
+    <div className={styles.container}>
+      <Accordion
+        entries={[
+          {
+            title: "Automate any workflow",
+            content: "Optimize your process with simple and secured CI/CD.",
+            img: "/accordion/01.jpg",
+          },
+          {
+            title: "Get up and running in seconds",
+            content:
+              "Start building instantly with a comprehensive dev environment in the cloud.",
+            img: "/accordion/02.jpg",
+          },
+          {
+            title: "Build on the go",
+            content:
+              "Manage projects and chat with GitHub Copilot from anywhere.",
+            img: "/accordion/03.jpg",
+          },
+          {
+            title: "Integrate the tools you love",
+            content:
+              "Sync with 17,000+ integrations and a growing library of Copilot Extensions.",
+            img: "/accordion/04.jpg",
+          },
+          {
+            title: "Share ideas and ask questions",
+            content:
+              "Create space for open-ended conversations alongside your project.",
+            img: "/accordion/05.jpg",
+          },
+        ]}
+      />
+    </div>
+  );
+}
